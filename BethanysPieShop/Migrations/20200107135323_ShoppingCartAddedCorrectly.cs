24d7a478ab743c07e391e3e6aacf992be8fe10a1
nspasov <nspasov22@gmail.com>
2020-03-16 13:46:25 +0200

    Add project files.

---
+++ BethanysPieShop/Migrations/20200107135323_ShoppingCartAddedCorrectly.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class ShoppingCartAddedCorrectly : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartITems_Pies_PieId",
+                table: "ShoppingCartITems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppingCartITems",
+                table: "ShoppingCartITems");
+
+            migrationBuilder.RenameTable(
+                name: "ShoppingCartITems",
+                newName: "ShoppingCartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingCartITems_PieId",
+                table: "ShoppingCartItems",
+                newName: "IX_ShoppingCartItems_PieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems",
+                column: "ShoppingCartItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Pies_PieId",
+                table: "ShoppingCartItems",
+                column: "PieId",
+                principalTable: "Pies",
+                principalColumn: "PieId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Pies_PieId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.RenameTable(
+                name: "ShoppingCartItems",
+                newName: "ShoppingCartITems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingCartItems_PieId",
+                table: "ShoppingCartITems",
+                newName: "IX_ShoppingCartITems_PieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppingCartITems",
+                table: "ShoppingCartITems",
+                column: "ShoppingCartItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartITems_Pies_PieId",
+                table: "ShoppingCartITems",
+                column: "PieId",
+                principalTable: "Pies",
+                principalColumn: "PieId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ BethanysPieShop/Models/IPieRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models
+{
+    public interface IPieRepository
+    {
+
+        IEnumerable<Pie> AllPies { get; }
+        IEnumerable<Pie> PiesOfTheWeek { get; }
+
+        Pie GetPieById(int pieId);
+
+    }
+}
